3ae6081a701ca2f8972829693223e330153f50c1
unknown <aishwaryam18@karunya.edu.in>
2021-08-07 19:15:31 +0530

    Assignment Completed

--- get-started-programs/l5-assignment/src/com/aishu/Tenth.java
+++ get-started-programs/l5-assignment/src/com/aishu/Tenth.java
@@ -1,4 +1,24 @@
 package com.aishu;
 
+import java.util.Scanner;
+
+//  Find the Largest of all until Zero
 public class Tenth {
+    public static void main(String[] args) {
+        System.out.println("Please Enter some random inputs:");
+        Scanner input = new Scanner(System.in);
+        int largest = 0;
+        while(true){
+            int num = input.nextInt();
+            if(largest<num)
+            {
+                largest=num;
+            }
+            if(num==0)
+            {
+                break;
+            }
+        }
+        System.out.println("The largest of all that was entered is "+largest);
+    }
 }
